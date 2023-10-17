4cb28274882837af2e3914685b3b15834472fca0
Daedalus <daedalus314@protonmail.com>
2023-10-17 21:42:17 +0200

    Documentation fastapi + addition of endpoints
    Documentation as docstrings and two endpoints added for the API keys:
    set and get.

--- gptstonks_api/main.py
+++ gptstonks_api/main.py
@@ -1,5 +1,6 @@
 import ast
 import asyncio
+import json
 import os
 import uuid
 
@@ -63,6 +64,10 @@ results_store = {}
 
 @app.on_event("startup")
 def init_data():
+    """Initial function called during the application startup.
+
+    It initializes the descriptions, definitions, classifiers, and templates for guidance.
+    """
     df = pd.read_csv(get_definitions_path(), sep=get_definitions_sep())
     df = df.dropna()
     app.definitions = df["Definitions"].tolist()
@@ -103,6 +108,15 @@ def init_data():
 
 @app.post("/process_query_async")
 async def process_query_async(request: Request):
+    """Asynchronous endpoint to start processing the given query. The processing runs in the
+    background, and a unique job ID is returned for fetching the result.
+
+    Args:
+        request (Request): FastAPI request object containing the query to be processed.
+
+    Returns:
+        dict: Contains a message confirming the start of processing and a unique job ID.
+    """
     data = await request.json()
     query = data.get("query")
 
@@ -115,6 +129,16 @@ async def process_query_async(request: Request):
 
 
 async def run_model_in_background(job_id: str, query: str):
+    """Background task to process the query using the STransformer and OpenBB. Stores the result in
+    the global results_store dictionary.
+
+    Args:
+        job_id (str): Unique identifier for the job.
+        query (str): User query to process.
+
+    Returns:
+        None
+    """
     with torch.inference_mode():
         _, scores, indices = app.stransformer.rank_k(query, k=3)
         if scores[0] < 0.45:
@@ -143,7 +167,11 @@ async def run_model_in_background(job_id: str, query: str):
             res = pd.DataFrame(res).dropna().to_dict(orient="dict")
         else:
             res = str(res)
-        results_store[job_id] = {"type": "data", "result_data": res, "body": code["answer"]}
+        results_store[job_id] = {
+            "type": "data",
+            "result_data": res,
+            "body": code["answer"],
+        }
     except Exception as e:
         print(e)
         results_store[job_id] = {
@@ -156,8 +184,56 @@ async def run_model_in_background(job_id: str, query: str):
 
 @app.get("/get_processing_result/{job_id}")
 async def get_processing_result(job_id: str):
+    """Endpoint to fetch the processing result using a job ID.
+
+    Args:
+        job_id (str): Unique identifier for the job.
+
+    Returns:
+        dict: Contains either the status of processing or the final result once completed.
+    """
     result = results_store.get(job_id)
     if result is None:
         return {"status": "processing"}
     else:
         return {"status": "completed", "result": result}
+
+
+@app.get("/get_api_keys")
+async def get_api_keys():
+    """Endpoint to retrieve the current API keys information.
+
+    Args:
+        None
+
+    Returns:
+        dict: Contains information about the current API keys.
+    """
+
+    keys = openbb.keys.get_keys_info()
+    return {"result": keys}
+
+
+@app.post("/set_api_keys")
+async def set_api_keys(request: Request):
+    """Endpoint to set new API keys. The new keys are set in the OpenBB SDK and also stored in a
+    JSON file.
+
+    Args:
+        request (Request): FastAPI request object containing the keys to set.
+
+    Returns:
+        dict: Status message confirming if keys were set correctly or not.
+    """
+
+    data = await request.json()
+    keys_list = data.get("keys_dict")
+    persist = data.get("persist") or True
+    show_output = data.get("show_output") or False
+    if keys_list is not None:
+        openbb.keys.set_keys(keys_dict=keys_list, persist=persist, show_output=show_output)
+        with open("./apikeys_list.json", "w") as keysFile:
+            json.dump(keys_list, keysFile)
+        return {"status": "Keys set correctly", "keys": keys_list}
+    else:
+        return {"status": "Key Format was not valid. API Key list not uploaded"}
