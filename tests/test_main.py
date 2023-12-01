76646b740b158de756438748709af8c82560022c
Daedalus <daedalus314@protonmail.com>
2023-12-01 22:34:30 +0100

    Modify response generation to be async
    The response is now generated using the async methods of llama-index and
    langchain. As a result, concurrent calls can be answered faster by the
    API.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,20 +7,12 @@ from ..gptstonks_api.main import app
 client = TestClient(app)
 
 
-def test_get_processing_result():
-    result = client.get("/get_processing_result/not_a_valid_job_id")
-
-    assert result.status_code == 200
-    assert "status" in result.json()
-    assert result.json()["status"] == "processing"
-
-
-@patch("asyncio.create_task")
-def test_process_query_async(mocked_create_task):
+def test_process_query_async():
     result = client.post(
         "/process_query_async", json={"query": "get the sentiment analysis of TSLA"}
     )
+    result_json = result.json()
 
     assert result.status_code == 200
-    mocked_create_task.assert_called_once()
-    assert "job_id" in result.json()
+    assert "type" in result_json
+    assert "body" in result_json
