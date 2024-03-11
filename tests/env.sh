28c6c576d0d2f0fb068ffea1be85de92a80740e2
Daedalus <daedalus314@protonmail.com>
2024-03-11 23:22:27 +0100

    Add multi-step reasoning
    A new tool, called World Knowledge, wraps the previous tools DuckDuckGo
    Search and Wikipedia and introduces multi-step reasoning to solve more
    complex queries and provide more accurate responses. It can be
    implemented with OpenAI function calling or with standard ReAct agents.

--- tests/env.sh
+++ tests/env.sh
@@ -4,4 +4,5 @@ export MONGO_URI=mongodb://localhost:27017
 export MONGO_DBNAME=mongodb
 export AUTOLLAMAINDEX_VSI_PATH="vsi:./gptstonks_api/data/openbb_v4.1.0_historical_vectorstoreindex_bgebaseen"
 export OPENBBCHAT_TOOL_DESCRIPTION="useful to get financial and investing data. Input should be the concrete data to retrieve, in natural language."
+export WORLD_KNOWLEDGE_TOOL_DESCRIPTION="useful to solve complex or incomplete financial questions and to search on the Internet current events, news and concrete financial datapoints."
 export LLM_MODEL_ID="llamacpp:./gptstonks_api/zephyr-7b-beta.Q4_K_M.gguf"
