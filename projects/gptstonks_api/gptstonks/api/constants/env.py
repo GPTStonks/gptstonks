8ec9cd01fe0a07efa6ac44d6bd856af358bd1876
Daedalus <daedalus314@protonmail.com>
2024-04-22 11:19:15 +0200

    Allow not specifying mandatory env variables
    Useful when importing from constants from other packages and not all
    mandatory vars are needed. A warning is generated instead of an error to
    make clear the env is not being specified.

--- projects/gptstonks_api/gptstonks/api/constants/env.py
+++ projects/gptstonks_api/gptstonks/api/constants/env.py
@@ -8,9 +8,18 @@ The list of env variables is provided in [Configuration with environment variabl
 """
 
 import os
+import warnings
 
-MONGO_URI: str = os.environ["MONGO_URI"]
-MONGO_DBNAME: str = os.environ["MONGO_DBNAME"]
+try:
+    MONGO_URI: str = os.environ["MONGO_URI"]
+except KeyError:
+    warnings.warn("MONGO_URI env variable not provided")
+    MONGO_URI = None
+try:
+    MONGO_DBNAME: str = os.environ["MONGO_DBNAME"]
+except KeyError:
+    warnings.warn("MONGO_DBNAME env variable not provided")
+    MONGO_DBNAME = None
 DEBUG_API: str | None = os.getenv("DEBUG_API")
 AUTOLLAMAINDEX_VSI_GDRIVE_URI: str | None = os.getenv("AUTOLLAMAINDEX_VSI_GDRIVE_URI")
 AUTOLLAMAINDEX_EMBEDDING_MODEL_ID: str = os.getenv(
@@ -22,7 +31,11 @@ AUTOLLAMAINDEX_SIMILARITY_POSTPROCESSOR_CUTOFF: float = float(
 AUTOLLAMAINDEX_REMOVE_METADATA_POSTPROCESSOR: str | None = os.getenv(
     "AUTOLLAMAINDEX_REMOVE_METADATA_POSTPROCESSOR"
 )
-AUTOLLAMAINDEX_VSI_PATH: str = os.environ["AUTOLLAMAINDEX_VSI_PATH"]
+try:
+    AUTOLLAMAINDEX_VSI_PATH: str = os.environ["AUTOLLAMAINDEX_VSI_PATH"]
+except KeyError:
+    warnings.warn("AUTOLLAMAINDEX_VSI_PATH env variable not provided")
+    AUTOLLAMAINDEX_VSI_PATH = None
 AUTOLLAMAINDEX_LLM_CONTEXT_WINDOW: int = int(os.getenv("AUTOLLAMAINDEX_LLM_CONTEXT_WINDOW", 4096))
 AUTOLLAMAINDEX_QA_TEMPLATE: str | None = os.getenv("AUTOLLAMAINDEX_QA_TEMPLATE")
 AUTOLLAMAINDEX_REFINE_TEMPLATE: str | None = os.getenv("AUTOLLAMAINDEX_REFINE_TEMPLATE")
@@ -45,7 +58,11 @@ AGENT_EARLY_STOPPING_METHOD: str = os.getenv("AGENT_EARLY_STOPPING_METHOD", "for
 LLM_TEMPERATURE: float = float(os.getenv("LLM_TEMPERATURE", 0.1))
 LLM_MAX_TOKENS: int = int(os.getenv("LLM_MAX_TOKENS", 256))
 LLM_TOP_P: float = float(os.getenv("LLM_TOP_P", 1.0))
-LLM_MODEL_ID: str = os.environ["LLM_MODEL_ID"]
+try:
+    LLM_MODEL_ID: str = os.environ["LLM_MODEL_ID"]
+except KeyError:
+    warnings.warn("LLM_MODEL_ID env variable not provided")
+    LLM_MODEL_ID = None
 LLM_CHAT_MODEL_SYSTEM_MESSAGE: str = os.getenv(
     "LLM_CHAT_MODEL_SYSTEM_MESSAGE", "You write concise and complete answers."
 )
@@ -57,8 +74,16 @@ LLM_HF_DEVICE_MAP: str | None = os.getenv("LLM_HF_DEVICE_MAP")
 LLM_HF_BITS: int = int(os.getenv("LLM_HF_GPTQ_BITS", 4))
 LLM_HF_DISABLE_EXLLAMA: bool = bool(os.getenv("LLM_HF_DISABLE_EXLLAMA", False))
 LLM_HF_TRUST_REMOTE_CODE: bool = bool(os.getenv("LLM_HF_TRUST_REMOTE_CODE"))
-OPENBBCHAT_TOOL_DESCRIPTION: str = os.environ["OPENBBCHAT_TOOL_DESCRIPTION"]
+try:
+    OPENBBCHAT_TOOL_DESCRIPTION: str = os.environ["OPENBBCHAT_TOOL_DESCRIPTION"]
+except KeyError:
+    warnings.warn("OPENBBCHAT_TOOL_DESCRIPTION env variable not provided")
+    OPENBBCHAT_TOOL_DESCRIPTION = None
 SEARCH_TOOL_DESCRIPTION: str | None = os.getenv("SEARCH_TOOL_DESCRIPTION")
 WIKIPEDIA_TOOL_DESCRIPTION: str | None = os.getenv("WIKIPEDIA_TOOL_DESCRIPTION")
 CUSTOM_GPTSTONKS_PREFIX: str | None = os.getenv("CUSTOM_GPTSTONKS_PREFIX")
-WORLD_KNOWLEDGE_TOOL_DESCRIPTION: str = os.environ["WORLD_KNOWLEDGE_TOOL_DESCRIPTION"]
+try:
+    WORLD_KNOWLEDGE_TOOL_DESCRIPTION: str = os.environ["WORLD_KNOWLEDGE_TOOL_DESCRIPTION"]
+except KeyError:
+    warnings.warn("WORLD_KNOWLEDGE_TOOL_DESCRIPTION env variable not provided")
+    WORLD_KNOWLEDGE_TOOL_DESCRIPTION = None
