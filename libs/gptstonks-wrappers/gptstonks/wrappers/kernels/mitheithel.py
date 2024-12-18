1571c2bc5dbbf073acc38658e0c46df832e55eb9
Daedalus <daedalus314@protonmail.com>
2024-12-18 18:26:29 +0100

    Fix login and register in Mitheithel API
    The keys were not properly configured for the current auth API of
    Mitheithel.

--- libs/gptstonks-wrappers/gptstonks/wrappers/kernels/mitheithel.py
+++ libs/gptstonks-wrappers/gptstonks/wrappers/kernels/mitheithel.py
@@ -1,11 +1,9 @@
 import os
-from getpass import getpass
 
 import httpx
 from pydantic import BaseModel, Field
 
 MITHEITHEL_AUTH_API: str = os.getenv("MITHEITHEL_AUTH_API", "https://api.gptstonks.net")
-MITHEITHEL_SERVICE_API: str = os.getenv("MITHEITHEL_AUTH_API", "https://service.gptstonks.net")
 
 
 class MitheithelAPI(BaseModel):
@@ -14,34 +12,34 @@ class MitheithelAPI(BaseModel):
     timeout: float = Field(default=20, description="seconds for the requests to expire")
     bearer_token: str | None = Field(default=None, description="bearer token to access service")
 
-    async def login(self, username: str, password: str | None = None) -> bool:
+    async def login(self, email: str, password: str) -> bool:
         """Login to Mitheithel API.
 
         Args:
-            username (`str`): Mitheithel username.
-            password (`str | None`): Mitheithel password, or None to prompt for it.
+            email (`str`): email to identify the account.
+            password (`str`): Mitheithel password, or None to prompt for it.
 
         Returns:
             `bool`: whether or not the login is successful.
         """
         service_login_url: str = f"{MITHEITHEL_AUTH_API}/login/password?is_service_account=true"
-        if password is None:
-            password = getpass(prompt="Mitheithel Account Password: ")
         async with httpx.AsyncClient(timeout=self.timeout) as client:
             res = await client.post(
-                service_login_url, json={"username": username, "password": password}
+                service_login_url, data={"username": email, "password": password}
             )
         self.bearer_token = (
             res.json().get("access_token") if res.status_code == httpx.codes.OK else None
         )
         return res.status_code == httpx.codes.OK
 
-    async def register(self, username: str, password: str | None = None) -> bool:
+    async def register(self, email: str, password: str, name: str, language: str = "en") -> bool:
         """Register to Mitheithel API.
 
         Args:
-            username (`str`): Mitheithel username.
-            password (`str | None`): Mitheithel password, or None to prompt for it.
+            email (`str`): email to identify the account.
+            password (`str`): Mitheithel password, or None to prompt for it.
+            name (`str`): Mitheithel username.
+            language (`str`): language to generate the responses. English by default.
 
         Returns:
             `bool`: whether or not the register is successful.
@@ -49,12 +47,10 @@ class MitheithelAPI(BaseModel):
         service_register_url: str = (
             f"{MITHEITHEL_AUTH_API}/register?plan=free&is_service_account=true"
         )
-        if password is None:
-            password = getpass(prompt="Mitheithel Account Password: ")
         async with httpx.AsyncClient(timeout=self.timeout) as client:
             res = await client.post(
                 service_register_url,
-                json={"username": username, "password": password},
+                json={"email": email, "password": password, "name": name, "language": language},
             )
         return res.status_code == httpx.codes.OK
 
@@ -64,7 +60,7 @@ class MitheithelAPI(BaseModel):
         Returns:
             `list[str] | None`: list of API keys to use with Mitheithel API. None if the status code is not OK (e.g., not authorized).
         """
-        read_service_account_url: str = f"{MITHEITHEL_SERVICE_API}/service/accounts/me"
+        read_service_account_url: str = f"{MITHEITHEL_AUTH_API}/service/accounts/me"
         async with httpx.AsyncClient(timeout=self.timeout) as client:
             res = await client.get(
                 read_service_account_url, headers={"Authorization": f"Bearer {self.bearer_token}"}
